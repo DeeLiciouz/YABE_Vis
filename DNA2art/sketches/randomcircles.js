b84dba9685714874b54b1537f4a50344204a3b5e
DeeLiciouz <54937933+DeeLiciouz@users.noreply.github.com>
2022-11-11 16:26:28 +0100

    update random circles

--- DNA2art/sketches/randomcircles.js
+++ DNA2art/sketches/randomcircles.js
@@ -1,20 +1,28 @@
 var circs = [];
-var numCircs = 900;
+var numCircs = 1200;
 function setup() {
+  console.log(random(0,14))
   createCanvas(1920,1080);
 
   for(i=0;i<numCircs;i++){
-    c = new circObj(random(30,60), i ) // generate a random sized circObj and store it's ID for later
+    c = new CircObj(SizeByData(random(20,200) ,str[Math.floor(random(0,str.length -1))]), i ) // generate a random sized circObj and store its ID for later
     circs.push(c); //add it to the array.
   }
 }
 
 function draw(){
   background(0);
-  circs.forEach((circ) => circ.disp());
+  circs.forEach((circ) => circ.display());
 }
 
-function circObj(d, id){
+function SizeByData(base, data) {
+  // 30
+  // const base = 100;
+
+  return base / ((data.x + data.y) / 2);
+}
+
+function CircObj(d, id){
   this.x = random(width)
   this.y = random(height)
   this.d = d
@@ -22,9 +30,11 @@ function circObj(d, id){
   this.color = color(random(255),random(255),random(255));
   this.hit = true;
 
-  this.disp = function(){
+  this.display = function(){
     noStroke();
     fill(this.color);
+    rotate(PI / 3);
+    rect(this.x, this.y, this.d, this.d);
     ellipse(this.x,this.y,this.d,this.d);
   }
 }
